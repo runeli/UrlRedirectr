c489fcfbf0ea8c0688dee09efef807395da12696
aleksanteri <aleksanteri.negru-vode@aalto.fi>
2015-01-19 21:15:58 +0200

    Refactored business logic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var url = require('url');
+var UrlHelper = require('./js/UrlHelper.js')
 var qs = require('querystring');
 var DB = require('./js/DB.js');
+var validUrl = require('valid-url');
 var db = new DB();
 
 
@@ -11,43 +13,50 @@ var server = http.createServer(function (req, res) {
 
     //POST /shorten
     if (req.method === 'POST' && requestPath === 'shorten') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
         var query = qs.parse(url.parse(req.url).query);
-        if (query.link) {
-            return res.end(db.set(query.link).toString());
+        var urlHelper = new UrlHelper(query.link);
+
+        res.writeHead(200, urlHelper.getHead('plain'));
+        
+        if (urlHelper.isValid()) {
+
+            //db.set() returns the given argument after pushing it to the database
+            return res.end(db.set(urlHelper.prependHttp().url).toString());
         }
 
     //GET /{id}
     } else if (req.method === 'GET' && !isNaN(requestPath)) {
 
-        var keyUrlPair = db.get(requestPath);
+        var urlHelper = new UrlHelper(db.get(requestPath));
         
-        if (keyUrlPair != undefined) {
-            var redirectUrl = keyUrlPair.url;
-
-            //Add http:// if missing e.g. 'www.google.com' -> 'http://www.google.com'
-            if (redirectUrl.substring(0, 7) != 'http://' && redirectUrl.substring(0, 8) != 'https://' ) {
-
-                redirectUrl = 'http://' + redirectUrl;
-
-            }
-
-            res.writeHead(301, { 'Location': redirectUrl });
+        if (urlHelper.isValid()) {
+            
+            res.writeHead(301, urlHelper.getHead());
             return res.end();
 
         }
 
     //show CV if requested
     } else if (requestPath === 'CV') {
-        res.writeHead(301, { 'Location': 'http://runeli.github.io/CV' });
+
+        var urlHelper = new UrlHelper('http://runeli.github.io/CV');
+
+        res.writeHead(301, urlHelper.getHead());
         return res.end();
 
     }
 
     //404 NOT FOUND
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    var urlHelper = new UrlHelper();
+    res.writeHead(404, urlHelper.getHead('plain'));
     return res.end('id not found');
+
 });
+
+//Argument: ´String´ containing an url or an ´Object´ containing a field for ´url´ {url:'http://reaktor.fi/'}
+
+
+
 
 server.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
