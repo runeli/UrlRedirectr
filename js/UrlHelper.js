5f96e85ce931bc4e727a9e15732679fa3db09389
aleksanteri <aleksanteri.negru-vode@aalto.fi>
2015-01-19 21:35:06 +0200

    removed unnecessary whitespace

--- js/UrlHelper.js
+++ js/UrlHelper.js
@@ -1,17 +1,11 @@
 var validUrl = require('valid-url');
 
 function UrlHelper(url) {
-
     if (typeof url === 'string') {
-
         this.url = url;
-
     } else if (typeof url !== 'undefined' && url.url !== 'undefined') {
-
         this.url = url.url;
-
     } else {
-
         this.url = undefined;
     }
 
@@ -24,28 +18,17 @@ UrlHelper.prototype.isValid = function () {
 UrlHelper.prototype.getHead = function (type) {
 
     if (type === 'location' || typeof type === 'undefined') {
-
         return { 'Location': this.prependHttp().url };
-
     } else if (type === 'plain') {
-
         return { 'Content-Type': 'text/plain' };
     }
-
-
-
 }
 
 UrlHelper.prototype.prependHttp = function () {
-
     if (this.url.substring(0, 7) !== 'http://' && this.url.substring(0, 8) !== 'https://') {
-
         this.url = 'http://' + this.url;
-
     }
-
     return this;
-
 }
 
 module.exports = UrlHelper;
